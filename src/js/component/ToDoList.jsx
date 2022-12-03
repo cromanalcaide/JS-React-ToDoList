dea24bdf60054a44d16813066801581a988414bd
cromanalcaide <cromanalcaide@gmail.com>
2022-12-03 10:57:46 +0000

    todo list working

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 
 const ToDoList = () => {
 
+const [allValues, setAllValues] = useState ([])
 const [inputValue, setInputValue] = useState ("");
-const [newValue, setNewValue] = useState ()
 
-// const prevList = () => {
-
-
-// }
-
-
-return
-<div><p>ffdfdfd</p></div>
-
-
+const addItem = (event) => {
+event.preventDefault();
+setAllValues(allValues => [...allValues, inputValue]);
 }
 
+const removeItem = allValues.filter(item => i !== item);
 
+    setList(newList);
+
+return (
+<div>
+    <form onSubmit={addItem}><input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue}/></form>
+    <div>
+        <ul>{allValues.map((task, i) => <li key={i}>{task}<button className="btn-danger" onClick={removeItem}>remove task</button></li>)}</ul>
+    </div>
+    {/* <button className="btn-danger" onClick={addItem}>remove task</button> */}
+    <div>{allValues.length == 0 ? "No tasks, add a task" : allValues.length + " items left"}</div>
+</div>
+
+)
+};
 export default ToDoList;
