c541c53c483f343a9f1299a291d2e8c1c5cf4608
cromanalcaide <cromanalcaide@gmail.com>
2022-12-03 12:27:51 +0000

    remove button working

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -10,17 +10,18 @@ event.preventDefault();
 setAllValues(allValues => [...allValues, inputValue]);
 }
 
-const removeItem = allValues.filter(item => i !== item);
-
-    setList(newList);
+const removeItem = (index) => {
+const newList = allValues.filter((task, id) => index !== id);
+setAllValues(newList);
+} 
+// qué coño pasa aquí
 
 return (
 <div>
     <form onSubmit={addItem}><input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue}/></form>
     <div>
-        <ul>{allValues.map((task, i) => <li key={i}>{task}<button className="btn-danger" onClick={removeItem}>remove task</button></li>)}</ul>
+        <ul>{allValues.map((task, index) => <div className="listItem"><li key={index}>{task}<button className="btn-danger" onClick={(event) => removeItem(index)}>Remove task</button></li></div>)}</ul>
     </div>
-    {/* <button className="btn-danger" onClick={addItem}>remove task</button> */}
     <div>{allValues.length == 0 ? "No tasks, add a task" : allValues.length + " items left"}</div>
 </div>
 
